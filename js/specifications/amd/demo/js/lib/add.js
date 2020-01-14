25521d13df8e350dd708d0b6332db13797e2f42e
warryy <warryy555@126.com>
2020-01-14 18:05:39 +0800

    bugfix js amd str->num

--- js/specifications/amd/demo/js/lib/add.js
+++ js/specifications/amd/demo/js/lib/add.js
@@ -2,7 +2,7 @@ define(['jquery'], function($) {
     function add(x, y) {
         let res = `module add: ${x} + ${y} equal: `;
         if ($.isNumeric(x) && $.isNumeric(y)) {
-            return x + y
+            return Number(x) + Number(y)
         } else {
             return 'NaN';
         }
